6b1f3c640da0722e497e150aa72fc0855a5fb493
PRANAV KUMAR <91466704+pranavkumar2001@users.noreply.github.com>
2022-06-13 19:36:45 +0530

    Time: 18 ms (5.23%), Space: 44.6 MB (16.57%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -4,16 +4,12 @@ class Solution
     {
         int n=tri.size();
         System.out.println(n);
-        // int[] front=new int[n];
-        // int[] current=new int[n];
-        // for(int j=0;j<n;j++)
-        //     front[j]=(int)tri.get(n-1).get(j);
         for(int i=n-2;i>=0;i--)
         {
             for(int j=0; j<tri.get(i).size();j++)
             {
                 int val=(int)tri.get(i).get(j)+Math.min((int)tri.get(i+1).get(j),(int)tri.get(i+1).get(j+1));
-                tri.get(i).set(j,val);
+                tri.get(i).set(j,val);  //to update value in 2d list
             }
         }
         return (int)tri.get(0).get(0);
