4872bff024806facdb0951b678a81f48003ac35d
PRANAV KUMAR <91466704+pranavkumar2001@users.noreply.github.com>
2022-06-18 10:42:05 +0530

    Time: 56 ms (20.55%), Space: 55.1 MB (18.27%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,22 +1,31 @@
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        if(coins.length == 1)
-            return amount % coins[0] == 0 ? amount / coins[0]: -1;
-        int[][] dp = new int[coins.length][amount + 1];
-        for(int[] i: dp)
-            Arrays.fill(i, 999999);
-        int ans = coinCount(coins.length - 1,coins, amount, dp);
-        return ans == 9999999 ? -1 : ans;
+class Solution 
+{
+    public int coinChange(int[] coins, int amount) 
+    {
+        // if(coins.length==1)
+        //     return amount%coins[0]==0?amount/coins[0]:-1;
+        
+        int[][] dp=new int[coins.length][amount+1];
+        for(int[] x:dp)
+            Arrays.fill(x,999999);
+        int ans=coin(coins,amount,coins.length-1,dp);
+        
+        return ans==9999999?-1:ans;
     }
-    private int coinCount(int ind,int[] coins,int amt,int[][] dp){
-        if(ind==0){
-            if(amt % coins[ind] == 0) return amt / coins[ind];
-            return 9999999;
+    
+    public int coin(int[] cn,int am,int i,int[][] dp)
+    {
+        if(i==0)
+        {
+             return am%cn[0]==0?am/cn[0]:9999999;
         }
-        if(dp[ind][amt] != 999999) return dp[ind][amt];
-        int notTake = coinCount(ind - 1, coins, amt, dp);
-        int take = Integer.MAX_VALUE;
-        if(coins[ind] <= amt) take = 1 + coinCount(ind, coins, amt - coins[ind], dp);
-        return dp[ind][amt] = Math.min(take, notTake);
+        if(dp[i][am]!=999999)
+            return dp[i][am];
+        int nottake=coin(cn,am,i-1,dp);
+        int take=Integer.MAX_VALUE;
+        if(cn[i]<=am)
+        take=1+coin(cn,am-cn[i],i,dp);
+        
+        return dp[i][am]=Math.min(take,nottake);
     }
 }
