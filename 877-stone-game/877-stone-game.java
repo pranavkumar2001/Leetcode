204b909bd92546747074b321bc09cb79d881c58a
PRANAV KUMAR <91466704+pranavkumar2001@users.noreply.github.com>
2022-06-27 21:27:47 +0530

    Time: 21 ms (22.96%), Space: 47.7 MB (38.07%) - LeetHub

--- 877-stone-game/877-stone-game.java
+++ 877-stone-game/877-stone-game.java
@@ -2,25 +2,27 @@ class Solution
 {
     public boolean stoneGame(int[] pi) 
     {
-        // if(pi.length==2)
-        //     return true;
-        // int sum=0;
-        // for(int x:pi)
-        //     sum+=x;
-        // int a=win(pi,1,pi.length-1,0);
-        // int b=win(pi,0,pi.length-2,0);
-        // int max=Math.max(a,b);
-        // return max>sum-max;
-        return true;
+         if(pi.length==2)
+            return true;
+        int[][] dp=new int[pi.length][pi.length];
+        int sum=0;
+        for(int x:pi)
+             sum+=x;
+         int a=win(pi,1,pi.length-1,0,dp);
+         int b=win(pi,0,pi.length-2,0,dp);
+         int max=Math.max(a,b);
+         return max>sum-max;
     }
     
-    public int win(int[] pi,int i,int j,int max)
+    public int win(int[] pi,int i,int j,int max,int[][] dp)
     {
          if(i>=j)
              return 0;
-         int a=pi[i]+win(pi,i+1,j,max);
-         int b=pi[j]+win(pi,i,j-1,max);
+         if(dp[i][j]!=0)
+             return dp[i][j];
+         int a=pi[i]+win(pi,i+1,j,max,dp);
+         int b=pi[j]+win(pi,i,j-1,max,dp);
         
-         return Math.max(a,b);
+         return dp[i][j]=Math.max(a,b);
     }
 }
