7323d789434894749e3272cbe22978ba3e8c4550
PRANAV KUMAR <91466704+pranavkumar2001@users.noreply.github.com>
2022-07-08 22:51:16 +0530

    Time: 9 ms (20.55%), Space: 47.8 MB (62.00%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -8,59 +8,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution 
-{
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
-    {
-        ListNode t1=reverse(l1);
-        ListNode t2=reverse(l2);
-        int carry=0;
-        ListNode ans=null,head=null;
-        while(t1!=null || t2!=null || carry>0)
-        {
-            ListNode temp=new ListNode(0);
-            int v=0;
-            if(t1!=null && t2!=null)
-              v=t1.val+t2.val+carry;
-            else if(t1!=null)
-              v=t1.val+carry;
-            else if(t2!=null)
-              v=t2.val+carry;
-            else if(carry>0)
-              v=carry;
-            temp.val=v%10;
-            carry=v/10;
-            if(ans!=null)
-            {
-                ans.next=temp;
-                ans=ans.next;
-            }
-            else
-            {
-                ans=temp;
-                head=temp;
-            }
-            if(t1!=null)
-             t1=t1.next;
-            if(t2!=null)
-             t2=t2.next;
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
         }
-
-        return reverse(head);
-    }
-    
-    public ListNode reverse(ListNode head)
-    {
-        if(head==null)
-            return head;
-        ListNode fast,prev=null,temp=head;
-        while(temp!=null)
-        {
-            fast=temp.next;
-            temp.next=prev;
-            prev=temp;
-            temp=fast;
+        
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
         }
-        return prev;
+        
+        return list.val == 0 ? list.next : list;
     }
 }
