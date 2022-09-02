0ce57b8dbfcd844660dc10f7c024b9f568d0b822
PRANAV KUMAR <91466704+pranavkumar2001@users.noreply.github.com>
2022-09-02 10:59:48 +0530

    Time: 3 ms (76.76%), Space: 60 MB (35.87%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -17,21 +17,19 @@ class Solution
 {
     public int goodNodes(TreeNode root) 
     {
-       int[] max={root.val};
+       int max=root.val;
        int count=preorder(root,max,0);
        return count;
     }
     
-    public int preorder(TreeNode root,int[] max,int count)
+    public int preorder(TreeNode root,int max,int count)
     {
        if(root==null)
            return count;
-       if(root.val>=max[0])
+       if(root.val>=max)
            count++;
-       max[0]=Math.max(max[0],root.val);
-       int temp=max[0];
+       max=Math.max(max,root.val);
        count=preorder(root.left,max,count);
-       max[0]=temp;
        count=preorder(root.right,max,count);
        return count;
     }
