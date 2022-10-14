731c573937cac87cdc1544a66dc0800b9c58ae55
PRANAV KUMAR <91466704+pranavkumar2001@users.noreply.github.com>
2022-10-14 19:58:03 +0530

    Time: 28 ms (24.94%), Space: 70.2 MB (7.08%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -7,34 +7,26 @@ class Solution
         int i=f.length-1,j=s.length-1;
         int carry=0;
         String add="";
-        while(i>=0 && j>=0)
+        while(i>=0 || j>=0)
         {
-            //System.out.println("c"+carry);
-            int x=f[i--]-'0',y=s[j--]-'0';
+            int x=0,y=0;
+            
+            if(i>=0)
+            x=f[i--]-'0';
+            
+            if(j>=0)
+            y=s[j--]-'0';
+            
             int sum=x+y+carry;
-            int ans=sum%10;
-            carry=sum/10;
-            //System.out.println(ans);
-            add=String.valueOf(ans)+add;
-        }
-        while(i>=0)
-        {
-            int x=f[i--]-'0';
-            int sum=x+carry;
+            
             int ans=sum%10;
             carry=sum/10;
             add=String.valueOf(ans)+add;
         }
-        while(j>=0)
-        {
-            int y=s[j--]-'0';
-            int sum=y+carry;
-            int ans=sum%10;
-            carry=sum/10;
-            add=String.valueOf(ans)+add;
-        }
+        
         if(carry!=0)
         add=String.valueOf(carry)+add;
+        
         return add;
     }
 }
